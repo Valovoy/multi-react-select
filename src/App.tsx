a45da9fc57cf7c9d6f152e54911fa8f8a0a183ba
Valovoy <aleksandr.valovoy@binerals.com>
2024-05-07 22:54:39 +0300

    created select with react-select library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Select, { ActionMeta } from "react-select";
+import cn from "classnames";
+import { DropIcon } from "./ui/svg";
+import styles from "./App.module.scss";
+
+interface OptionType {
+  value: string;
+  label: string;
+}
+
+const options: OptionType[] = [
+  { value: "item1", label: "Элемент 1" },
+  { value: "item2", label: "Элемент 2" },
+  { value: "item3", label: "Элемент 3" },
+  { value: "item4", label: "Элемент 4" },
+  { value: "item5", label: "Элемент 5" },
+  { value: "item6", label: "Элемент 6" },
+  { value: "item7", label: "Элемент 7" },
+  { value: "item8", label: "Элемент 8" },
+];
 
 function App() {
+  const [selectedOptions, setSelectedOptions] = React.useState<
+    readonly OptionType[]
+  >([]);
+
+  const handleChange = (
+    option: readonly OptionType[],
+    actionMeta: ActionMeta<OptionType>
+  ) => {
+    setSelectedOptions(option);
+  };
+
+  const formatOptionLabel = ({ label, value }: OptionType) => (
+    <>
+      <input
+        type="checkbox"
+        checked={selectedOptions.some((opt) => opt.value === value)}
+        className={styles.checkbox}
+        onChange={() => {}}
+      />
+      <span className={styles.label}>{label}</span>
+    </>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Select
+        isMulti
+        value={selectedOptions}
+        onChange={handleChange}
+        options={options}
+        formatOptionLabel={formatOptionLabel}
+        closeMenuOnSelect={false}
+        hideSelectedOptions={false}
+        isClearable={false}
+        components={{
+          IndicatorSeparator: () => null, // hide separator line near dropdown btn
+          DropdownIndicator: () => <DropIcon className={styles.icon} />, // change icon dropdown btn
+          MultiValueRemove: () => null, // hide X btn for each selected element
+        }}
+        className={styles.select}
+        classNames={{
+          // added own classNames
+          control: () => styles.control,
+          placeholder: () => styles.placeholder,
+          dropdownIndicator: () => styles.dropdownIndicator,
+          option: (state) =>
+            cn(styles.option, state.isSelected && styles.isSelected),
+          multiValue: () => styles.multiValue,
+        }}
+        placeholder="Choose"
+      />
     </div>
   );
 }
---
+++ src/ui/svg/DropIcon.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgDropIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 12 12"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M10.096 7.644a.38.38 0 0 1-.346.231h-7.5a.375.375 0 0 1-.265-.64l3.75-3.75a.375.375 0 0 1 .53 0l3.75 3.75a.38.38 0 0 1 .081.409"
+    />
+  </svg>
+);
+export default SvgDropIcon;
